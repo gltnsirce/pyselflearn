21a76857ca8a78fcb87c6d63e92e5299181f8a3a
Yiming <41096999+gltnsirce@users.noreply.github.com>
2019-07-30 09:31:39 +0800

    Add files via upload

---
+++ a0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+
+a = eval(input('Enter a number, a ='))
+b = eval(input('Enter a number, b ='))
+
+print(type(a))
+print(type(b))
+
+print(a*b)
+
---
+++ a1.py
@@ -0,0 +1,3 @@
+# -*- coding: utf8 -*-
+
+# This is a demo script for python 3.6.x self learning
