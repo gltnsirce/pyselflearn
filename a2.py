a9e9b190f9cda434278c1bd0fbd76d741438d5a1
Yiming <41096999+gltnsirce@users.noreply.github.com>
2019-07-30 11:43:44 +0800

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,14 @@
+# -*- coding= utf-8 -*-
+
+import time
+
+print('This is a demo for Python self-learning.')
+print(time.time())
+
+def hello():
+    ''''''
+    return "hello world"
+
+if __name__ == "__main__":
+    print(hello())
+    print(hello() == 'hello world')
