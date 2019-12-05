cdff12b7d863c681f0134dfd989c69e4a8375f75
Yiming <41096999+gltnsirce@users.noreply.github.com>
2019-12-05 21:50:44 +0800

    Batch IP address ping test
    EE_IP_resource.py

---
+++ ping_ip_addr.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import subprocess
+import os
+import sys
+
+beginIP = input('Enter the starting IP address:')
+endIP = input('Enter the ending IP address:')
+
+with open(os.devnull, "wb") as limbo:
+    for ping in range(int(beginIP),int(endIP)):
+        addr = "10.62.91." + str(ping)
+        res = subprocess.Popen(['ping', '-c', '1', "-n" , "-w" , "2" ,addr],
+                stdout = limbo,
+                stderr = limbo).wait()
+        if res:
+            print( addr, '\x1b[1;31m'+'inactive'+'\x1b[0m')
+        else:
+            print( addr, '\x1b[1;32m'+'active'+ '\x1b[0m')
